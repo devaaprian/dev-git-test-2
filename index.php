6490f585a531ab663a8e175560f63f6e49cd249d
devaaprian <theprojessa@gmail.com>
2020-06-06 20:09:16 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum totam, vero quo neque placeat cupiditate in. Commodi officia placeat consequatur at labore repellendus praesentium vel accusamus eius totam? Atque, veritatis.</p>
 </body>
 </html>

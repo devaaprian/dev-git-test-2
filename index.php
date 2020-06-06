95b64769c586a3b5b279bd3e772b453eda7a897a
devaaprian <theprojessa@gmail.com>
2020-06-06 20:15:56 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum totam, vero quo neque placeat cupiditate in. Commodi officia placeat consequatur at labore repellendus praesentium vel accusamus eius totam? Atque, veritatis.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
